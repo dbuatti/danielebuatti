2067cee4c2d834e270e4553ac67f35d82e17dc3e
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-27 18:08:18 +1000

    [dyad] Set up Supabase authentication with a session provider and login page - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,30 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
-import { ThemeProvider } from "./components/ThemeProvider"; // Import ThemeProvider
+import { ThemeProvider } from "./components/ThemeProvider";
+import { SessionContextProvider } from "./components/SessionContextProvider"; // Import SessionContextProvider
+import LoginPage from "./pages/Login"; // Import LoginPage
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> {/* Add ThemeProvider here */}
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          {/* Navbar and Footer are now part of the LandingPage for simplicity,
-              but can be moved here if more routes are added later. */}
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <SessionContextProvider> {/* Wrap with SessionContextProvider */}
+            <Routes>
+              <Route path="/login" element={<LoginPage />} /> {/* Login route */}
+              <Route path="/" element={<LandingPage />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </SessionContextProvider>
         </BrowserRouter>
       </TooltipProvider>
-    </ThemeProvider> {/* Close ThemeProvider */}
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
