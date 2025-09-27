02a1e8dd2aa0f26db1f93bd27fbd66607ebb6a01
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-27 18:08:56 +1000

    Reverted all changes back to version 3db0d12547ef35279305b94407b696015f16f895

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,27 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
-import { ThemeProvider } from "./components/ThemeProvider";
-import { SessionContextProvider } from "./components/SessionContextProvider"; // Import SessionContextProvider
-import LoginPage from "./pages/Login"; // Import LoginPage
+import { ThemeProvider } from "./components/ThemeProvider"; // Import ThemeProvider
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> {/* Add ThemeProvider here */}
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <SessionContextProvider> {/* Wrap with SessionContextProvider */}
-            <Routes>
-              <Route path="/login" element={<LoginPage />} /> {/* Login route */}
-              <Route path="/" element={<LandingPage />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </SessionContextProvider>
+          {/* Navbar and Footer are now part of the LandingPage for simplicity,
+              but can be moved here if more routes are added later. */}
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </BrowserRouter>
       </TooltipProvider>
-    </ThemeProvider>
+    </ThemeProvider> {/* Close ThemeProvider */}
   </QueryClientProvider>
 );
 
