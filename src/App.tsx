c39ec93ff8fa3567f9a32b722a5c8d62f1962cc6
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-27 22:07:57 +1000

    [dyad] Added scroll-to-top functionality on route changes - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPageV3 from './pages/LandingPageV3'; // Changed from Index to LandingPageV3
+import LandingPageV3 from './pages/LandingPageV3';
 import LivePianoServicesPage from './pages/LivePianoServicesPage';
-import { Toaster } from 'sonner'; // Changed from react-hot-toast to sonner
+import { Toaster } from 'sonner';
+import ScrollToTop from './components/ScrollToTop'; // Import the new ScrollToTop component
 
 function App() {
   return (
     <Router>
       <Toaster />
+      <ScrollToTop /> {/* Wrap Routes with ScrollToTop */}
       <Routes>
-        <Route path="/" element={<LandingPageV3 />} /> {/* Updated to LandingPageV3 */}
+        <Route path="/" element={<LandingPageV3 />} />
         <Route path="/live-piano-services" element={<LivePianoServicesPage />} />
       </Routes>
     </Router>
