d543c3a0b0fbaf9010b5acaf594ae2b55320a345
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-28 10:11:02 +1000

    [dyad] Increased Instagram feed iframe height to 1000px. - wrote 1 file(s)

--- src/components/pages/landing-page-v3/InstagramFeedSection.tsx
+++ src/components/pages/landing-page-v3/InstagramFeedSection.tsx
@@ -14,7 +14,7 @@ const InstagramFeedSection: React.FC = () => {
             scrolling="no"
             allowTransparency={true}
             className="lightwidget-widget"
-            style={{ width: '100%', border: 0, overflow: 'hidden', height: '500px' }} // Set height to 500px
+            style={{ width: '100%', border: 0, overflow: 'hidden', height: '1000px' }} // Set height to 1000px
             title="Instagram Feed"
           ></iframe>
         </div>
