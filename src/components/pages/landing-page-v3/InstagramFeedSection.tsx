a5b1ac711c288e9e2ca0f9752cca236fb8a25acf
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-28 11:23:38 +1000

    [dyad] Set Instagram widget height to 1600px. - wrote 1 file(s)

--- src/components/pages/landing-page-v3/InstagramFeedSection.tsx
+++ src/components/pages/landing-page-v3/InstagramFeedSection.tsx
@@ -14,7 +14,7 @@ const InstagramFeedSection: React.FC = () => {
             scrolling="no"
             allowTransparency={true}
             className="lightwidget-widget"
-            style={{ width: '100%', border: 0 }}
+            style={{ width: '100%', border: 0, height: '1600px' }} {/* Added height: '1600px' */}
             title="Instagram Feed"
           ></iframe>
         </div>
