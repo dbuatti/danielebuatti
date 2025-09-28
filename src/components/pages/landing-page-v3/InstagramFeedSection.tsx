4a63398360d81708c091ea25580d2e7a89904a86
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-28 10:07:30 +1000

    [dyad] Reduced Instagram feed iframe height to 300px. - wrote 1 file(s)

--- src/components/pages/landing-page-v3/InstagramFeedSection.tsx
+++ src/components/pages/landing-page-v3/InstagramFeedSection.tsx
@@ -14,7 +14,7 @@ const InstagramFeedSection: React.FC = () => {
             scrolling="no"
             allowTransparency={true}
             className="lightwidget-widget"
-            style={{ width: '100%', border: 0, overflow: 'hidden', height: '400px' }} // Adjusted height to 400px
+            style={{ width: '100%', border: 0, overflow: 'hidden', height: '300px' }} // Adjusted height to 300px
             title="Instagram Feed"
           ></iframe>
         </div>
