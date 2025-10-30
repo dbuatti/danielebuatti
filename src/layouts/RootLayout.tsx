ff2c8474e638365e2c41c9bbce24a7ba2190e4a5
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-10-31 10:38:19 +1100

    [dyad] Removed the unused import of ScrollToTop from RootLayout.tsx to fix a TypeScript error. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,12 +2,10 @@
 
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import ScrollToTop from '@/components/ScrollToTop';
 
 const RootLayout: React.FC = () => {
   return (
     <>
-      <ScrollToTop />
       <Outlet />
     </>
   );
