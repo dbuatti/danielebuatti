3403103bf79f060fe4325e89cf67df802581b7ec
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-10-30 19:51:36 +1100

    [dyad] Added the 'About' page link to the main navigation menu. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -2,6 +2,7 @@ export const navLinks = [
   { name: "Home", href: "/" },
   { name: "Coaching", href: "/coaching" }, // New direct link for coaching services
   { name: "Projects & Resources", href: "/projects-resources" }, // Renamed from 'Programs'
+  { name: "About", href: "/about" }, // Added About page link
   { name: "Contact", href: "/#contact" },
 ];
 
