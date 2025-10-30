0b0857579674f4382006bfd5286dfe9bdf5736c5
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-10-30 20:05:56 +1100

    [dyad] wrote 9 file(s) + extra files edited outside of Dyad

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useEffect } from 'react';
+import { useEffect, type PropsWithChildren } from 'react'; // Import PropsWithChildren
 import { useLocation } from 'react-router-dom';
 
-const ScrollToTop: React.FC = () => {
+const ScrollToTop: React.FC<PropsWithChildren> = ({ children }) => { // Accept children prop
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  return null;
+  return <>{children}</>; // Render children
 };
 
 export default ScrollToTop;
