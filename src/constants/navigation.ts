dd589179907888db8b6b1ef1b53cb0680ccc6f79
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-10-30 19:51:54 +1100

    [dyad] Reordered the navigation links to place 'About' between 'Home' and 'Coaching'. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,8 +1,8 @@
 export const navLinks = [
   { name: "Home", href: "/" },
+  { name: "About", href: "/about" }, // Moved About page link
   { name: "Coaching", href: "/coaching" }, // New direct link for coaching services
   { name: "Projects & Resources", href: "/projects-resources" }, // Renamed from 'Programs'
-  { name: "About", href: "/about" }, // Added About page link
   { name: "Contact", href: "/#contact" },
 ];
 
