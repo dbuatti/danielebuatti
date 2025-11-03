7c063e36b82abc931da3daf0f78e9a73c8ee5aca
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-11-03 18:40:29 +1100

    [dyad] Fixed 404 error on admin page by wrapping the application with SessionContextProvider. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx"; // App now exports the RouterProvider
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx"; // Import ThemeProvider
+import { SessionContextProvider } from "./components/SessionContextProvider.tsx"; // Import SessionContextProvider
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme" attribute="class">
-    <App />
+    <SessionContextProvider>
+      <App />
+    </SessionContextProvider>
   </ThemeProvider>
 );
