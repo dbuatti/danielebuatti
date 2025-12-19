efd205fde39df4ce74d69ea22412e9ff2a182fc7
[dyad] <daniele.buatti@gmail.com>
2025-12-19 21:02:39 +1100

    Updated src/constants/navigation.ts

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,17 +1,7 @@
 export const navLinks = [
-
-{ name: "Home", href: "/" },
-
-{ name: "About", href: "/about" }, // Moved About page link
-
-{ name: "Coaching", href: "/coaching" }, // New direct link for coaching services
-
-{ name: "Projects & Resources", href: "/projects-resources" }, // Renamed from 'Programs'
-
-{ name: "Contact", href: "/contact" }, // Updated to dedicated page
-
+  { name: "Home", href: "/" },
+  { name: "Coaching", href: "/coaching" },
+  { name: "Resources", href: "/projects-resources" }, 
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
 ];
-
-
-
-// Note: The 'Book a session' CTA is handled directly in Navbar.tsx and will be updated there.
