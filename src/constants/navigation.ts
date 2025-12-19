02f6d04edfbe5273d94f16bdcad654490ba7f596
[dyad] <daniele.buatti@gmail.com>
2025-12-19 21:00:37 +1100

    Updated src/constants/navigation.ts

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,8 +1,7 @@
 export const navLinks = [
   { name: "Home", href: "/" },
   { name: "Coaching", href: "/coaching" },
-  { name: "Live Piano", href: "/live-piano-services" }, 
   { name: "Resources", href: "/projects-resources" }, 
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
-];  
+];
