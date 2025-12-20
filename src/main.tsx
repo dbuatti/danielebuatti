752ebeca178a71446f349a8aae83c1c60e0d3bd7
[dyad] <daniele.buatti@gmail.com>
2025-12-20 13:50:26 +1100

    [dyad] Fixing TypeScript errors - wrote 20 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx"; // App now exports the RouterProvider
+import App from "./App";
 import "./globals.css";
-// Removed ThemeProvider import
 
 createRoot(document.getElementById("root")!).render(
-  // Removed ThemeProvider wrapper
     <App />
 );
